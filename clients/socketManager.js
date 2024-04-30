14a36e3b7d5633bf3ca052e28f162e1bbb66f6ae
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-04-30 16:36:47 -0500

    Refactor

--- clients/socketManager.js
+++ clients/socketManager.js
@@ -5,8 +5,8 @@ const { io } = require('socket.io-client');
 const rdsNameSpaceUrl = process.env.NAMESPACE_URL; 
 const socket = io(rdsNameSpaceUrl);
 
-function joinRoom(roomName, callback) {
-  socket.emit('JOIN', roomName, callback);
+function joinRoom(roomName) {
+  socket.emit('JOIN', roomName);
 }
 
 function emitEvent(event, data) {
