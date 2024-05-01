5e87475537085a83bda08c1370c21c962294e2b7
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-05-01 13:09:31 -0500

    Add ability to connect to server and join multiple customer rooms

---
+++ clients/restaurant/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// Load environment variables
+require('dotenv').config({ path: './.env'});
+
+// Imports
+const socketManager = require('../socketManager');
+// const handleFoodPrepAndNotifyDriver = require('../restaurant/handler');
+
+// Contain list of customer rooms joined
+let joinedRooms = new Set();
+
+// Request the server to send back all customer rooms
+socketManager.emitEvent('GET_CUSTOMER_ROOMS');
+
+// Handle receiving list of existing customer rooms
+socketManager.listenForEvent('EXISTING_CUSTOMER_ROOMS', (customerRooms) => {
+  console.log('Recieved customer rooms', customerRooms);
+  customerRooms.forEach(room => {
+    if (!joinedRooms.has(room)) {
+      joinRoomAndRequestOrders(room);
+      joinedRooms.add(room);
+    }
+  });
+});
+
+// Join newly created rooms and request orders(in event driver joins namespace before vendors)
+socketManager.listenForEvent('NEW_CUSTOMER_ROOM', (customerRoom) => {
+  if (!joinedRooms.has(customerRoom)) {
+    joinRoomAndRequestOrders(customerRoom);
+    joinedRooms.add(customerRoom);
+  }
+});
+
+// Todo - Listen for food order ready
+// socketManager.listenForEvent('FOOD_ORDER_READY', (foodOrder) => {
+//   processFoodOrder(foodOrder);
+// });
+
+//*------ Helper Functions ------*/
+
+// Join room and request orders from queue if any
+function joinRoomAndRequestOrders(room) {
+  socketManager.joinRoom(room);
+  console.log(`Joined room ${room}`);
+  socketManager.emitEvent('GET_FOOD_ORDERS', {customerRoom: room});
+}
+
+// Process order (prep food and notify driver ready for pickup)
+// function processFoodOrder(order) {
+//   let socket = socketManager.socket;
+//   setTimeout(() => {
+//     handleFoodPrepAndNotifyDriver.simulateFoodPrep(socket, order);
+//   }, 2000);
+//   setTimeout(() => {
+//     handleFoodPrepAndNotifyDriver.simulateNotifyDriverToPickUpOrder(socket, order);
+//   }, 4000);
+// }
+
+
+
