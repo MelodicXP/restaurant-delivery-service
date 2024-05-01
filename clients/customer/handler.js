d344ecf172e4ea06168c034a53892c7352cff940
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-05-01 17:27:22 -0500

    Listen for status updates on food

--- clients/customer/handler.js
+++ clients/customer/handler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function sendFoodOrderToRestaurant (socket, foodOrder) {
-  console.log(`---------------Food Order ID: ${foodOrder.orderID} sent to Restaurant-------------`);
+  console.log(`\n Food Order ID: ${foodOrder.orderID} sent to Restaurant`);
   socket.emit('FOOD_ORDER_READY', foodOrder);
 }
 
