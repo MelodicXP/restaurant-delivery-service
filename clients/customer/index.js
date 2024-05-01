78091e5554eed8649efbd766393ddd3f8fd36978
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-05-01 17:28:11 -0500

    Listen for status updates on food

--- clients/customer/index.js
+++ clients/customer/index.js
@@ -16,10 +16,23 @@ const customer = orderCreator.createCustomer();
 // Join room using socket manager
 socketManager.joinRoom(customer.customerRoom); 
 
+// Listen for notification/event of PREPARING_FOOD
+socketManager.listenForEvent('PREPARING_FOOD', (foodOrder) => {
+  console.log(
+    `${foodOrder.customerName} your food order# ${foodOrder.orderID} is being prepared`);
+});
+
+// Listen for notification/event of READY_FOR_PICKUP
+socketManager.listenForEvent('READY_FOR_PICKUP', (foodOrder) => {
+  console.log(`${foodOrder.customerName} a driver has been notified to pick up your food order# ${foodOrder.orderID}`);
+});
+
 // Schedule food orders to be sent regularly (every 11s)
 setInterval(() => {
   const foodOrder = orderCreator.createFoodOrderForCustomer(customer);
-  console.log('The following food order has been created:', foodOrder);
+  console.log(`\n\n---------------New Food Order for ${customer.name}---------------`);
+
+  console.log('\n The following food order has been created:', foodOrder);
 
   let socket = socketManager.socket;
   orderHandler.sendFoodOrderToRestaurant(socket, foodOrder);
