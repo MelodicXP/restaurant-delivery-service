db3de0ac098e9834f90306910cfc1e4a8fb397c0
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-05-01 17:28:54 -0500

    Reformat console log ouput

--- clients/restaurant/handler.js
+++ clients/restaurant/handler.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const simulateFoodPrep = (socket, foodOrder) => {
-  console.log(`RESTAURANT: Received food order ${foodOrder.orderID} for ${foodOrder.customerName}, commencing preparation of order.`);
+  console.log(`\n\n---------------New Food Order for ${foodOrder.customerName}---------------`);
+  console.log(`\n RESTAURANT: Received food order# ${foodOrder.orderID} for ${foodOrder.customerName}, commencing preparation of food items.`);
   console.table(foodOrder.items);
   socket.emit('PREPARING_FOOD', foodOrder);
 };
 
 const simulateNotifyDriverToPickUpOrder = (socket, foodOrder) => {
-  console.log(`RESTAURANT: food order ${foodOrder.orderID} is ready, driver has been notified to pick up order`);
+  console.log(`\n RESTAURANT: food order# ${foodOrder.orderID} is ready, driver has been notified to pick up order`);
   socket.emit('READY_FOR_PICKUP', foodOrder);
 };
 
