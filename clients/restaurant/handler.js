68c7b49bcbd71862943d408c1bc9d50cc75e1d93
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-05-03 08:31:44 -0500

    Update console log ouput

--- clients/restaurant/handler.js
+++ clients/restaurant/handler.js
@@ -2,13 +2,13 @@
 
 const simulateFoodPrep = (socket, foodOrder) => {
   console.log(`\n\n---------------New Food Order for ${foodOrder.customerName}---------------`);
-  console.log(`\n RESTAURANT: Received food order# ${foodOrder.orderID} for ${foodOrder.customerName}, commencing preparation of food items.`);
+  console.log(`\nRESTAURANT: Received food order# ${foodOrder.orderID} for ${foodOrder.customerName}, commencing preparation of food items.`);
   console.table(foodOrder.items);
   socket.emit('PREPARING_FOOD', foodOrder);
 };
 
 const simulateNotifyDriverToPickUpOrder = (socket, foodOrder) => {
-  console.log(`\n RESTAURANT: food order# ${foodOrder.orderID} is ready, driver has been notified to pick up order`);
+  console.log(`\nRESTAURANT: food order# ${foodOrder.orderID} is ready, driver has been notified to pick up order`);
   socket.emit('READY_FOR_PICKUP', foodOrder);
 };
 
